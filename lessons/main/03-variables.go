30d719473729e159cc85d385c23132cfb24e5ccc
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2020-11-24 12:21:06 +0100

    variables

---
+++ lessons/main/03-variables.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var declares a variable, then comes the variable type
+	var a string = "initial"
+	fmt.Println(a)
+
+	// type can be inferred
+	var a2 = "initial"
+	fmt.Println(a2)
+
+	// more variables can be declared in a single line
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	// variables without value are zero-valued
+	var d bool // false
+	fmt.Println(d)
+	var e int // zero
+	fmt.Println(e)
+
+	// a shorthand for `var f string = "apple"`
+	f := "apple"
+	fmt.Println(f)
+}
