c2333173b83a9551cf1b633f61502b46eabb2450
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2020-11-24 12:16:55 +0100

    values

---
+++ lessons/main/02-values.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// string concatenation
+	fmt.Println("go" + "lang")
+
+	// integer
+	fmt.Println("1+1 =", 1+1)
+
+	// float
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// boolean
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
