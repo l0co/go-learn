5f84cefa26d208f91cec95e8e5f7fdabfbaa18a5
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2020-12-01 15:45:24 +0100

    mutex

--- lessons/main/36-atomic-counter-and-mutex.go
+++ lessons/main/36-atomic-counter-and-mutex.go
@@ -10,8 +10,10 @@ func main() {
 
 	var ops uint64
 	var nonAtomicOps uint64
+	var nonAtomicOpsWithMutext uint64
 
 	var wg sync.WaitGroup
+	var mutex = sync.Mutex{}
 
 	for i := 0; i < 50; i++ {
 		wg.Add(1)
@@ -20,6 +22,9 @@ func main() {
 			for c := 0; c < 1000; c++ {
 				atomic.AddUint64(&ops, 1)
 				nonAtomicOps++ // there's a gap between read and write ( i = i + 1 ) so we would have a race condition here
+				mutex.Lock()
+				nonAtomicOpsWithMutext++ // there's will be no such a gap because we sync ops with mutex
+				mutex.Unlock()
 			}
 			wg.Done()
 		}()
@@ -29,7 +34,9 @@ func main() {
 
 	fmt.Println("ops:", ops)
 	fmt.Println("nonAtomicOps:", nonAtomicOps)
+	fmt.Println("nonAtomicOpsWithMutext:", nonAtomicOpsWithMutext)
 
 	// ops: 50000
 	// nonAtomicOps: 22351
+	// nonAtomicOpsWithMutext: 50000
 }
